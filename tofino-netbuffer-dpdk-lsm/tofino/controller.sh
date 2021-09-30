6ec5b23a434ff138dc4afc08ff9a2eb4946280bb
Cyril Sheng <592521098@qq.com>
2021-09-30 06:08:36 +0000

    update tofino/controller

---
+++ tofino-netbuffer-dpdk-lsm/tofino/controller.sh
@@ -0,0 +1,31 @@
+if [ $1 == "setup" ]
+then
+	#python3 controller/controller.py >/dev/null 2>&1 &
+	python3 controller/pull_listener.py >/dev/null 2>&1 &
+	#python3 controller/controller.py &
+	#python3 controller/pull_listener.py &
+elif [ $1 == "cleanup" ]
+then
+	pids=$(ps -aux | grep "controller.py" | grep -v "grep" | awk '{print $2}')
+	if [ ${#pids[@]} -gt 0 ]
+	then
+		for i in ${!pids[@]}
+		do
+			if [ "${pids[i]}x" != "x" ]
+			then
+				kill -15 ${pids[i]}
+			fi
+		done
+	fi
+	pids=$(ps -aux | grep "pull_listener.py" | grep -v "grep" | awk '{print $2}')
+	if [ ${#pids[@]} -gt 0 ]
+	then
+		for i in ${!pids[@]}
+		do
+			if [ "${pids[i]}x" != "x" ]
+			then
+				kill -15 ${pids[i]}
+			fi
+		done
+	fi
+fi
