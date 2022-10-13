07eba5a2b45cb023a35a16598b50e15190db3ba0
Theodore Puyang <hpuyang@connect.ust.hk>
2022-10-13 19:39:50 +0800

    fix scripts bug

--- distcache/sync_file.sh
+++ distcache/sync_file.sh
@@ -1,8 +1,8 @@
 source ../scripts/common.sh
 filename=$1
 
-scp -r $filename ${USER}@bf1:~/${SWITCH_ROOTPATH}distcache/$filename
-scp -r $filename ${USER}@bf3:~/${SWITCH_ROOTPATH}distcache/$filename
+scp -r $filename ${USER}@bf1:~/${SWITCH_ROOTPATH}/distcache/$filename
+scp -r $filename ${USER}@bf3:~/${SWITCH_ROOTPATH}/distcache/$filename
 scp -r $filename ${USER}@dl13:~/${SERVER_ROOTPATH}/distcache/$filename
 scp -r $filename ${USER}@${SECONDARY_CLIENT}:~/${CLIENT_ROOTPATH}/distcache/$filename
 scp -r $filename ${USER}@dl16:~/${SERVER_ROOTPATH}/distcache/$filename
