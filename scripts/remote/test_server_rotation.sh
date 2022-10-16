4edfe7ca70fd2af0e568448945c47755c4b8f2fb
siyuansheng <592521098@qq.com>
2022-10-16 20:11:58 +0800

    split p0 from test_server_rotation

--- scripts/remote/test_server_rotation.sh
+++ scripts/remote/test_server_rotation.sh
@@ -12,24 +12,7 @@ fi
 
 ##### Part 0 #####
 
-if [ with_controller -eq 1 ]
-then
-	# NOTE: if w/ in-switch cache, finish warmup phase by launching servers of correpsonding method + warmup_client + stopping servers
-	echo "[part 0] pre-admit hot keys into switch before server rotation"
-
-	echo "launch storage servers of ${DIRNAME}"
-	source scripts/remote/launchservertestebed.sh
-	sleep 10s
-
-	echo "pre-admit hot keys"
-	cd ${DIRNAME}
-	./warmup_client
-	cd ..
-	sleep 10s
-
-	echo "stop storage servers of ${DIRNAME}"
-	source scripts/remote/stopservertestbed.sh
-fi
+source scripts/remote/test_server_rotation_p0.sh
 
 ##### Part 1 #####
 
