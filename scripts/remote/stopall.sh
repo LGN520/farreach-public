12b05b2f1c8f0f4b1381649c82b218fa2a8781c2
sysheng <sysheng21@cse.cuhk.edu.hk>
2023-05-07 22:24:51 -0400

    update AE instructions for hardware resource usage; update stopall to kill warmup_client; polish readme

--- scripts/remote/stopall.sh
+++ scripts/remote/stopall.sh
@@ -3,6 +3,8 @@ then
 	source scripts/common.sh
 fi
 
+echo "For ${DIRNAME}:"
+
 # Stop and kill clients
 echo "Stop and kill clients..."
 source bash scripts/local/localstop.sh ycsb >/dev/null 2>&1
@@ -13,10 +15,18 @@ ssh ${USER}@${SECONDARY_CLIENT} "cd ${CLIENT_ROOTPATH}; bash scripts/local/local
 # Stop and kill servers and controller (w/ reflector)
 echo "Stop and kill servers..."
 bash scripts/remote/stopservertestbed.sh
-echo "Stop exps if any..."
-source scripts/local/localkill.sh run_exp >/dev/null 2>&1
-echo "Stop server rotation if any..."
-bash scripts/remote/stop_server_rotation.sh
 
 # Stop and kill switch
+echo "Stop and kill switch data plane and switch OS (as well as daemon processes)..."
 ssh -i /home/${USER}/${SWITCH_PRIVATEKEY} root@${LEAFSWITCH} "cd ${SWITCH_ROOTPATH}/${DIRNAME}; bash localscripts/stopswitchtestbed.sh"
+
+# Stop and kill processes in main client
+echo "Stop run_exps if any..."
+source scripts/local/localkill.sh run_exp >/dev/null 2>&1
+echo "Stop warmup_client is any"
+source scripts/local/localkill.sh warmup_client >/dev/null 2>&1
+echo "Stop test_server_rotation if any..."
+bash scripts/remote/stop_server_rotation.sh
+echo "Stop test_dynamic if any..."
+souce scripts/local/localkill.sh test_dynamic >/dev/null 2>&1
+echo "Stop "
