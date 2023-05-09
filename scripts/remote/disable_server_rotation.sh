cefa6606ef209e1eea3bc71867b61a2ca7463824
sysheng <sysheng21@cse.cuhk.edu.hk>
2023-05-08 21:55:14 -0400

    update scripts to enable and disable server rotation

--- scripts/remote/disable_server_rotation.sh
+++ scripts/remote/disable_server_rotation.sh
@@ -7,3 +7,15 @@ fi
 
 sed -i "s/^#define SERVER_ROTATION/\/\/#define SERVER_ROTATION/g" common/helper.h
 source scripts/remote/sync_file.sh common helper.h
+
+echo "[INFO] recompile code for FarReach"
+source scripts/remote/setmethod.sh farreach
+source scripts/remote/makeallsoft.sh
+
+echo "[INFO] recompile code for NoCache"
+source scripts/remote/setmethod.sh nocache
+source scripts/remote/makeallsoft.sh
+
+echo "[INFO] recompile code for NetCache"
+source scripts/remote/setmethod.sh netcache
+source scripts/remote/makeallsoft.sh
--- scripts/remote/enable_server_rotation.sh
+++ scripts/remote/enable_server_rotation.sh
@@ -5,5 +5,18 @@ then
 	source scripts/common.sh
 fi
 
+echo "[INFO] enable server rotation"
 sed -i "s/^\/\/#define SERVER_ROTATION/#define SERVER_ROTATION/g" common/helper.h
 source scripts/remote/sync_file.sh common helper.h
+
+echo "[INFO] recompile code for FarReach"
+source scripts/remote/setmethod.sh farreach
+source scripts/remote/makeallsoft.sh
+
+echo "[INFO] recompile code for NoCache"
+source scripts/remote/setmethod.sh nocache
+source scripts/remote/makeallsoft.sh
+
+echo "[INFO] recompile code for NetCache"
+source scripts/remote/setmethod.sh netcache
+source scripts/remote/makeallsoft.sh
