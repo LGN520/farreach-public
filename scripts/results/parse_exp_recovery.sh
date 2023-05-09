496124e9bf4769ed4fff0c8d0262edab420978e4
sysheng <sysheng21@cse.cuhk.edu.hk>
2023-05-09 01:33:59 -0400

    fix typos in scripts/results/

--- scripts/results/parse_exp_recovery.sh
+++ scripts/results/parse_exp_recovery.sh
@@ -2,16 +2,31 @@
 
 if [ $# -ne 1 ]
 then
-	echo "Usage: bash scripts/reulsts/parse_exp_throughput.sh <filename>"
+	echo "Usage: bash scripts/reulsts/parse_exp_recovery.sh <filename>"
 	exit
 fi
 tmp_outputfile=$1
 
-echo "Get throughput results of FarReach if any"
-$ awk -v flag=0 'flag == 0 && /\[exp1\]\[farreach\]\[.*\] sync/ {flag = 1; print $0; next} flag == 1 && /aggregate throughput/ {flag = 0; print $0; next}' ${tmp_outputfile}
+echo "Get server-side and switch-side crash recovery time of round 0 if any"
+awk -v flag=0 'flag == 0 && /\[exp9\]\[0\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Server total/ {flag = 0; print $0; next}' ${tmp_outputfile}
+awk -v flag=0 'flag == 0 && /\[exp9\]\[0\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Switch total/ {flag = 0; print $0; next}' ${tmp_outputfile}
 
-echo "Get throughput results of NoCache if any"
-$ awk -v flag=0 'flag == 0 && /\[exp1\]\[nocache\]\[.*\] sync/ {flag = 1; print $0; next} flag == 1 && /aggregate throughput/ {flag = 0; print $0; next}' ${tmp_outputfile}
+echo "Get server-side and switch-side crash recovery time of round 1 if any"
+awk -v flag=0 'flag == 0 && /\[exp9\]\[1\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Server total/ {flag = 0; print $0; next}' ${tmp_outputfile}
+awk -v flag=0 'flag == 0 && /\[exp9\]\[1\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Switch total/ {flag = 0; print $0; next}' ${tmp_outputfile}
 
-echo "Get throughput results of NetCache if any"
-$ awk -v flag=0 'flag == 0 && /\[exp1\]\[netcache\]\[.*\] sync/ {flag = 1; print $0; next} flag == 1 && /aggregate throughput/ {flag = 0; print $0; next}' ${tmp_outputfile}
+echo "Get server-side and switch-side crash recovery time of round 2 if any"
+awk -v flag=0 'flag == 0 && /\[exp9\]\[2\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Server total/ {flag = 0; print $0; next}' ${tmp_outputfile}
+awk -v flag=0 'flag == 0 && /\[exp9\]\[2\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Switch total/ {flag = 0; print $0; next}' ${tmp_outputfile}
+
+echo "Get server-side and switch-side crash recovery time of round 3 if any"
+awk -v flag=0 'flag == 0 && /\[exp9\]\[3\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Server total/ {flag = 0; print $0; next}' ${tmp_outputfile}
+awk -v flag=0 'flag == 0 && /\[exp9\]\[3\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Switch total/ {flag = 0; print $0; next}' ${tmp_outputfile}
+
+echo "Get server-side and switch-side crash recovery time of round 4 if any"
+awk -v flag=0 'flag == 0 && /\[exp9\]\[4\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Server total/ {flag = 0; print $0; next}' ${tmp_outputfile}
+awk -v flag=0 'flag == 0 && /\[exp9\]\[4\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Switch total/ {flag = 0; print $0; next}' ${tmp_outputfile}
+
+echo "Get server-side and switch-side crash recovery time of round 5 if any"
+awk -v flag=0 'flag == 0 && /\[exp9\]\[5\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Server total/ {flag = 0; print $0; next}' ${tmp_outputfile}
+awk -v flag=0 'flag == 0 && /\[exp9\]\[5\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Switch total/ {flag = 0; print $0; next}' ${tmp_outputfile}
--- scripts/results/parse_exp_value_size.sh
+++ scripts/results/parse_exp_value_size.sh
@@ -2,31 +2,16 @@
 
 if [ $# -ne 1 ]
 then
-	echo "Usage: bash scripts/reulsts/parse_exp_recovery.sh <filename>"
+	echo "Usage: bash scripts/reulsts/parse_exp_value_size.sh <filename>"
 	exit
 fi
 tmp_outputfile=$1
 
-echo "Get server-side and switch-side crash recovery time of round 0 if any."
-$ awk -v flag=0 'flag == 0 && /\[exp9\]\[0\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Server total/ {flag = 0; print $0; next}' ${tmp_outputfile}
-$ awk -v flag=0 'flag == 0 && /\[exp9\]\[0\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Switch total/ {flag = 0; print $0; next}' ${tmp_outputfile}
+echo "Get throughput results of FarReach if any"
+awk -v flag=0 'flag == 0 && /\[exp6\]\[farreach\]\[.*\] sync/ {flag = 1; print $0; next} flag == 1 && /aggregate throughput/ {flag = 0; print $0; next}' ${tmp_outputfile}
 
-echo "Get server-side and switch-side crash recovery time of round 1 if any."
-$ awk -v flag=0 'flag == 0 && /\[exp9\]\[1\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Server total/ {flag = 0; print $0; next}' ${tmp_outputfile}
-$ awk -v flag=0 'flag == 0 && /\[exp9\]\[1\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Switch total/ {flag = 0; print $0; next}' ${tmp_outputfile}
+echo "Get throughput results of NoCache if any"
+awk -v flag=0 'flag == 0 && /\[exp6\]\[nocache\]\[.*\] sync/ {flag = 1; print $0; next} flag == 1 && /aggregate throughput/ {flag = 0; print $0; next}' ${tmp_outputfile}
 
-echo "Get server-side and switch-side crash recovery time of round 2 if any."
-$ awk -v flag=0 'flag == 0 && /\[exp9\]\[2\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Server total/ {flag = 0; print $0; next}' ${tmp_outputfile}
-$ awk -v flag=0 'flag == 0 && /\[exp9\]\[2\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Switch total/ {flag = 0; print $0; next}' ${tmp_outputfile}
-
-echo "Get server-side and switch-side crash recovery time of round 3 if any."
-$ awk -v flag=0 'flag == 0 && /\[exp9\]\[3\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Server total/ {flag = 0; print $0; next}' ${tmp_outputfile}
-$ awk -v flag=0 'flag == 0 && /\[exp9\]\[3\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Switch total/ {flag = 0; print $0; next}' ${tmp_outputfile}
-
-echo "Get server-side and switch-side crash recovery time of round 4 if any."
-$ awk -v flag=0 'flag == 0 && /\[exp9\]\[4\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Server total/ {flag = 0; print $0; next}' ${tmp_outputfile}
-$ awk -v flag=0 'flag == 0 && /\[exp9\]\[4\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Switch total/ {flag = 0; print $0; next}' ${tmp_outputfile}
-
-echo "Get server-side and switch-side crash recovery time of round 5 if any."
-$ awk -v flag=0 'flag == 0 && /\[exp9\]\[5\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Server total/ {flag = 0; print $0; next}' ${tmp_outputfile}
-$ awk -v flag=0 'flag == 0 && /\[exp9\]\[5\]\[.*\] Get recovery time/ {flag = 1; print $0; next} flag == 1 && /Switch total/ {flag = 0; print $0; next}' ${tmp_outputfile}
+echo "Get throughput results of NetCache if any"
+awk -v flag=0 'flag == 0 && /\[exp6\]\[netcache\]\[.*\] sync/ {flag = 1; print $0; next} flag == 1 && /aggregate throughput/ {flag = 0; print $0; next}' ${tmp_outputfile}
