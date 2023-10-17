fe32d7de628394f94aa58591174bfdadb8782e36
zetta-cai <cjztcl@163.com>
2023-10-17 16:33:46 +0800

    create localscripts for farreach bmv2

---
+++ farreach/localscriptsbmv2/fetchall_all2switch.sh
@@ -0,0 +1,24 @@
+set -x
+if [ "x${is_common_included}" != "x1" ]
+then
+	cd ..
+	source scripts/common.sh
+	if [ "x${DIRNAME}" != "xfarreach" ]
+	then
+		echo "[ERROR] you should change DIRNAME as farreach in scripts/common.sh before running fetchsnapshotandmaxseq_controllerandserver2switch.sh"
+		exit
+	fi
+	cd ${DIRNAME}
+fi
+
+# 
+
+if [ "x${DIRNAME}" != "xfarreach" ]
+then
+	echo "[ERROR] DIRNAME should be farreach in scripts/common.sh for fetchall_all2switch.sh"
+	exit
+fi
+
+mkdir -p /tmp/${DIRNAME}
+rm -r /tmp/${DIRNAME}/*
+
